9116092f2f977869e42db72a0b35746a3a4a2464
TuYuWang <18759280805@163.com>
2018-04-16 16:11:23 +0800

    Flexbox

--- ReactNativeDemo/index.ios.js
+++ ReactNativeDemo/index.ios.js
@@ -10,10 +10,11 @@ import {
   View
 } from 'react-native';
 
-export default class ReactNativeDemo123 extends Component {
+export default class ReactNativeDemoFlexDirection extends Component {
   render() {
     return (
-      <View>
+      //flexDirection可以决定布局的主轴。子元素水平轴(row)方向排列，沿着竖直轴(column)方向排列
+      <View style={{flex: 1, flexDirection: 'row'}}>
         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
         <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
         <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
@@ -22,16 +23,40 @@ export default class ReactNativeDemo123 extends Component {
   }
 }
 
-class ReactNativeDemo456 extends Component {
+class ReactNativeDemoJustifyContent extends Component {
   render() {
     return (
-      //使用flex:1来指定某个组件扩张以撑满所有剩余的空间
-      //有多个并列的子组件使用了flex:1，则这些子组件会平分父容器中剩余的空间
-      //flex值不一样，则谁的值更大，谁占据剩余空间的比例就更大
-      <View style={{flex: 1}}>
-          <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-          <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-          <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+      //justifyContent可以决定其子元素沿着主轴的排列方式
+      //flex-start、center、flex-end、space-around以及space-between。
+      <View style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'center',
+      }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+      </View>
+    );
+  }
+}
+
+class ReactNativeDemoAlignItems extends Component {
+  render() {
+    return (
+      //alignItems决定与主轴垂直的轴的排列方式
+      //flex-start、center、flex-end以及stretch
+      //stretch选项生效的话，子元素在次轴方向上不能有固定的尺寸。
+      //以下面的代码为例：只有将子元素样式中的width: 50去掉之后，alignItems: 'stretch'才能生效
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'stretch',
+      }}>
+        <View style={{ height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{ height: 50, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
@@ -39,4 +64,4 @@ class ReactNativeDemo456 extends Component {
 
 // 注册应用(registerComponent)后才能正确渲染
 // 注意：只把应用作为一个整体注册一次，而不是每个组件/模块都注册
-AppRegistry.registerComponent('ReactNativeDemo', () => ReactNativeDemo456);
+AppRegistry.registerComponent('ReactNativeDemo', () => ReactNativeDemoAlignItems);
