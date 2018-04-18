a3dea86499a63d691d9e22632547dfd3b26d34ec
TuYuWang <18759280805@163.com>
2018-04-18 14:40:13 +0800

    ListView

--- ReactNativeDemo/index.ios.js
+++ ReactNativeDemo/index.ios.js
@@ -8,12 +8,34 @@ import React, { Component } from 'react';
 import { 
   Text,
   AppRegistry,
+  ListView,
+  View,
 } from 'react-native';
 
+//ListView组件用于显示一个垂直的滚动列表，其中的元素之间结构近似而仅数据不同。
+//ListView优先渲染屏幕上可见的元素。
+//dataSource是列表的数据源，而renderRow则逐个解析数据源中的数据，然后返回一个设定好格式的组件来渲染。
+//类似UITableView
 export default class ReactNativeDemo extends Component {
+
+  constructor(props) {
+    super(props);
+    //rowHasChanged
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([
+        'John', 'joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+      ])
+    };
+  }
   render() {
     return (
-      <Text>Hello World!</Text>
+      <View style={{paddingTop: 22}}>
+        <ListView 
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text>{rowData}</Text>}
+        />
+      </View>
     );
   }
 }
