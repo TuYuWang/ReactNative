6fb9cff662b3e52a008b058fe5a87b341ca44108
TuYuWang <18759280805@163.com>
2018-04-20 11:40:13 +0800

    Update

--- ReactNativeDemo/index.ios.js
+++ ReactNativeDemo/index.ios.js
@@ -16,6 +16,8 @@ import {
   ScrollView,
 } from 'react-native';
 
+
+//this 问题
 const ViewExample = require('./ViewExample');
 
 class EmptyPage extends Component {
@@ -322,4 +324,45 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('ReactNativeDemo', () => ReactNativeDemo);
+
+//
+
+class NavigatorIOSApp extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+        initialRoute={{
+          component: MyScene,
+          title: 'My Initial Scene',
+        }}
+        style={{flex: 1}}
+      />
+    );
+  }
+}
+
+class MyScene extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    navigator: PropTypes.object.isRequired,
+  }
+
+  _onForward = () => {
+    this.props.navigator.push({
+      title: 'Scene ' + nextIndex,
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Current Scene: { this.props.title }</Text>
+        <TouchableHighlight onPress={this._onForward}>
+          <Text>Tap me to load the next scene</Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
+
+AppRegistry.registerComponent('ReactNativeDemo', () => NavigatorIOSApp);
