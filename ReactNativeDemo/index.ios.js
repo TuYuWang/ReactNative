d7c630ad9c0d40fea46fbdc073fa6dfaa428720a
TuYuWang <18759280805@163.com>
2018-04-20 18:29:00 +0800

    update

--- ReactNativeDemo/index.ios.js
+++ ReactNativeDemo/index.ios.js
@@ -8,12 +8,54 @@ import React, { Component } from 'react';
 import { 
   Text,
   AppRegistry,
+  View,
+  ActivityIndicator,
+  FlatList,
 } from 'react-native';
 
 export default class ReactNativeDemo extends Component {
+
+  constructor(props) {
+    super(props);
+  
+    this.state = { isLoading: true };
+  }
+
+  componentDidMount(){
+    return fetch('http://suggest.taobao.com/sug?code=utf-8&q=婴儿纸尿裤')
+      .then(response => response.json()) 
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.result,
+
+        }, function(){
+          console.log(responseJson);
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
+
+    if (this.state.isLoading) {
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
     return (
-      <Text>Hello World!</Text>
+      <View style={{flex: 1, paddingTop: 20}}>
+        <FlatList 
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item[0]}, {item[1]}</Text>}
+          keyExtractor={(item, index) => index}
+        />
+      </View>
     );
   }
 }
